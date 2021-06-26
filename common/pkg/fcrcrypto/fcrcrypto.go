caeae610437b89aa042645b3fff2b5e7b0d4ba4a
wcgcyx <wcgcyx@gmail.com>
2021-06-27 04:47:36 +1000

    Update year

--- common/pkg/fcrcrypto/fcrcrypto.go
+++ common/pkg/fcrcrypto/fcrcrypto.go
@@ -4,7 +4,7 @@ Package fcrcrypto - location for cryptographic tools to perform common operation
 package fcrcrypto
 
 /*
- * Copyright 2020 ConsenSys Software Inc.
+ * Copyright 2021 ConsenSys Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
--- common/pkg/fcrserver/fcrserver.go
+++ common/pkg/fcrserver/fcrserver.go
@@ -8,7 +8,7 @@ package fcrserver
 import "github.com/wcgcyx/fc-retrieval/common/pkg/fcrmessages"
 
 /*
- * Copyright 2020 ConsenSys Software Inc.
+ * Copyright 2021 ConsenSys Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
