0327cff5505fa99aa8a699c298d9f709d0d5a857
wcgcyx <wcgcyx@gmail.com>
2021-07-11 03:11:42 +1000

    Fix admin token

--- gateway/docker-entrypoint.sh
+++ gateway/docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+mkdir -p .fc-retrieval/gateway/
+file=".fc-retrieval/gateway/admin.key"
+echo "deaa5112ef638de9b9120cf65736dedef0f6642d4af42fabe65c55c2a2c806ff" > $file
+
 export CONTAINER_IP="$(awk 'END{print $1}' /etc/hosts)"
 
 echo "Container IP: $CONTAINER_IP"
