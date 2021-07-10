7e9e2a9f24ca16c9a1b289c0679083edc1619600
wcgcyx <wcgcyx@gmail.com>
2021-07-11 03:22:19 +1000

    Update offer_publish_handler.go

--- gateway/internal/api/p2papi/offer_publish_handler.go
+++ gateway/internal/api/p2papi/offer_publish_handler.go
@@ -87,10 +87,11 @@ func OfferPublishHandler(reader fcrserver.FCRServerRequestReader, writer fcrserv
 			}
 			cidVal, _ := big.NewInt(0).SetString(hex.EncodeToString(cidHash), 16)
 			if cidVal.Cmp(min) >= 0 && cidVal.Cmp(max) <= 0 {
-				logging.Debug("Offer contains cid %v, within range [%v, %v], added to storage", cidHash, minStr, maxStr)
+				logging.Debug("Offer contains cid %v, within range [%v, %v], added to storage", hex.EncodeToString(cidHash), minStr, maxStr)
 				break
 			}
 		}
+		logging.Debug("Offer does not contain cid within range [%v, %v], ignore", minStr, maxStr)
 	} else {
 		logging.Debug("Gateway stores every offer, added to storage")
 		c.OfferMgr.AddOffer(offer)
