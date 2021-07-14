2229499950773b6739bdf33f9bfdea95c1435f67
wcgcyx <wcgcyx@gmail.com>
2021-07-14 12:15:10 +1000

    Add testing

--- gateway/docker-entrypoint.sh
+++ gateway/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-mkdir -p .fc-retrieval/gateway/
-file=".fc-retrieval/gateway/admin.key"
+mkdir -p .test/.fc-retrieval/gateway/
+file=".test/.fc-retrieval/gateway/admin.key"
 echo "deaa5112ef638de9b9120cf65736dedef0f6642d4af42fabe65c55c2a2c806ff" > $file
 
 export CONTAINER_IP="$(awk 'END{print $1}' /etc/hosts)"
--- provider/docker-entrypoint.sh
+++ provider/docker-entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-mkdir -p .fc-retrieval/provider/files
-file=".fc-retrieval/provider/admin.key"
+mkdir -p .test/.fc-retrieval/provider/files
+file=".test/.fc-retrieval/provider/admin.key"
 echo "deaa5112ef638de9b9120cf65736dedef0f6642d4af42fabe65c55c2a2c806ff" > $file
-testFile1=".fc-retrieval/provider/files/test1.txt"
+testFile1=".test/.fc-retrieval/provider/files/test1.txt"
 echo "This is the test file 1" > $testFile1
-testFile2=".fc-retrieval/provider/files/test2.txt"
+testFile2=".test/.fc-retrieval/provider/files/test2.txt"
 echo "This is the test file 2" > $testFile2
-testFile3=".fc-retrieval/provider/files/test3.txt"
+testFile3=".test/.fc-retrieval/provider/files/test3.txt"
 echo "This is the test file 3" > $testFile3
 
 export CONTAINER_IP="$(awk 'END{print $1}' /etc/hosts)"
